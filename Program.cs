434dc0ec681ec2738a4b3ccdcf98aac50b8828b7
Robert MacLean <robert@sadev.co.za>
2020-12-09 11:54:06 +0200

    step 2

--- Program.cs
+++ Program.cs
@@ -1,13 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
 var numbers = (await File.ReadAllLinesAsync("data.txt")).Select(n => Convert.ToInt64(n)).ToArray();
-var range = 25;
-Console.WriteLine($"The problem is with {FindEncodingErrorNumber()}");
+// Console.WriteLine($"The problem is with {FindEncodingErrorNumber()}");
+Console.WriteLine($"Break the encryption with {FindEncryptionWeakness()}");
+
+long FindEncryptionWeakness()
+{
+    var target = 36845998; // determined from calling FindEncodingErrorNumber
+    for (var startIndex = 0; startIndex < numbers.Length - 1; startIndex++)
+    {
+        var set = new List<long>();
+        for (var testIndex = startIndex; testIndex < numbers.Length; testIndex++)
+        {
+            set.Add(numbers[testIndex]);
+            if (set.Sum() == target)
+            {
+                var smallest = set.Min();
+                var largest = set.Max();
+                return smallest + largest;
+            }
+        }
+    }
+
+    throw new Exception("Unbreakable!");
+}
 
 long FindEncodingErrorNumber()
 {
+    var range = 25;
+
     for (var targetIndex = range; targetIndex < numbers.Length; targetIndex++)
     {
         var target = numbers[targetIndex];
